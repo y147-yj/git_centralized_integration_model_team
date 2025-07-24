73617ada7c52a2ef84964e9cc44dc31c288b41c8
ubeen2 <yb2179@naver.com>
2025-07-24 15:58:23 +0900

    개발2

--- git_centralized_integration_model_team.java
+++ git_centralized_integration_model_team.java
@@ -1,3 +1,6 @@
 public class Calculator {
     public static double performOperation(double num1, double num2, char operator) {
         double result = 0.0;
+		
+		public class CalculatorInput {
+    public static void getInputAndCalculate() {
