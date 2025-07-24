30a9f3357bec62e3a52986c776b2442548bda772
ubeen2 <yb2179@naver.com>
2025-07-24 15:59:26 +0900

    개발3

--- git_centralized_integration_model_team.java
+++ git_centralized_integration_model_team.java
@@ -4,3 +4,11 @@ public class Calculator {
 		
 		public class CalculatorInput {
     public static void getInputAndCalculate() {
+		 Scanner scanner = new Scanner(System.in);
+
+        System.out.println("간단한 콘솔 계산기 프로그램");
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        double num2 = scanner.nextDouble();
